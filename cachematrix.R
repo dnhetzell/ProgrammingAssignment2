6411a64db8ed64f2cde4352cb9e65f7ff8d7cb0a
Dave Hetzell <david.hetzell@gmail.com>
2015-04-08 21:31:24 -0400

    Assigmnent 2 1st Attempt

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Pair of functions which return a special matrix with attached Cache informaton
+##  and also caches inverse of the matrix, assumes matrix passed through is square
+## and inversable
 
-## Write a short comment describing this function
+## Creates Special Matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y ##caches matrix as a variable for recall
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(x){ m <<- x} ##caches matrix when called meant to cache matrix which function has taken place
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## creates and stores inverse of matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x' assumes x is the special matrix created using
+        ## makeCacheMatrix function
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setsolve(m)
 }
